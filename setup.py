ed09841cd1cf69488a2a2130c913d24d958bd757
ww <ww@styx.org>
2011-02-16 00:01:39 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.12.4'
+version = '0.12.5'
 try:
     from mercurial import ui, hg, error
     repo = hg.repository(ui.ui(), ".")
