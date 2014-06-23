9c9b00aee1967865c87327284b80f4769f16d7ff
Marc-Antoine Parent <maparent@acm.org>
2014-06-23 18:04:02 -0400

    refactoring error

--- virtuoso/vstore.py
+++ virtuoso/vstore.py
@@ -545,7 +545,7 @@ class VirtuosoNamespaceManager(NamespaceManager):
 
     def bind_virtuoso(self, session, prefix, namespace):
         self.bind(prefix, namespace)
-        if namespace not in self.v_uris:
+        if prefix not in self.v_prefixes:
             session.execute("XML_SET_NS_DECL('%s', '%s', 2)" % (
                 prefix, namespace))
             self.v_prefixes[prefix] = namespace
